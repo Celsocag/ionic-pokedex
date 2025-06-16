7d23b00e478af32f76d3f5d3f5e8e002e1088579
Celsocag <celsoalbugo@gmail.com>
2025-06-16 16:51:19 -0300

    feat: estrutura inicial com páginas, serviços e models

---
+++ src/app/pages/details/details.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  standalone: false,
+  templateUrl: './details.page.html',
+  styleUrls: ['./details.page.scss'],
+})
+export class DetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
