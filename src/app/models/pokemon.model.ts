d28fd12d0ceafcedab85fc015296fbc25a472e3c
Celsocag <celsoalbugo@gmail.com>
2025-06-16 17:30:16 -0300

    feat: adiciona método para pegar imagem ao model de pokemon

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -2,7 +2,11 @@ export class Pokemon {
   constructor(public name: string, public url: string) {}
 
   getId(): string {
-    const parts = this.url.split("/").filter(Boolean);
+    const parts = this.url.split('/').filter(Boolean);
     return parts[parts.length - 1];
+  }
+
+  getImageUrl(): string {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.getId()}.png`;
   }
 }
